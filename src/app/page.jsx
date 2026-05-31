548c0cdf9319a10a52499fbb479afdeae1f5bec3
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-05-31 21:55:25 +0600

    Updated Home Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div className="p-10">
-      <button className="btn btn-primary">Click Me</button>
-    </div>
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">My First DaisyUI Card</h2>
+          <p>Learning Next.js + Tailwind + DaisyUI</p>
+
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
