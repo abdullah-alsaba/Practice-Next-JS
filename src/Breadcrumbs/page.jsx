60d5122929166d778c6751670df39749f5a69790
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-06-01 22:12:02 +0600

    Done Breadcrumbs

---
+++ src/Breadcrumbs/page.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+const page = () => {
+    return (
+      <div>
+        <div className="breadcrumbs text-sm">
+          <ul>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Documents</a>
+            </li>
+            <li>Add Document</li>
+          </ul>
+        </div>
+      </div>
+    );
+};
+
+export default page;
