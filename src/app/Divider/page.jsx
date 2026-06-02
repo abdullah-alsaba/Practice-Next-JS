7809d280651117a6ea1c7586bc743a9121b3b293
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-06-02 22:15:06 +0600

    Add Divider

---
+++ src/app/Divider/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const page = () => {
+    return (
+      <div>
+        <div className="flex w-full flex-col">
+          <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+            content
+          </div>
+          <div className="divider">OR</div>
+          <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+            content
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default page;
