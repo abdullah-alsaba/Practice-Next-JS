7b96a91c96e5c585901b44e3f99ee5f902d9d63e
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-06-02 22:07:47 +0600

    Add Badge

---
+++ src/app/Badge/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="badge badge-primary">Primary</div>
+      <div className="badge badge-secondary">Secondary</div>
+      <div className="badge badge-accent">Accent</div>
+      <div className="badge badge-neutral">Neutral</div>
+      <div className="badge badge-info">Info</div>
+      <div className="badge badge-success">Success</div>
+      <div className="badge badge-warning">Warning</div>
+      <div className="badge badge-error">Error</div>
+    </div>
+  );
+};
+
+export default page;
