cd82fc955ed75adfe69cca8a29acc5f629d10fde
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-06-02 22:16:09 +0600

    Add Browser

---
+++ src/app/Browser/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const page = () => {
+    return (
+      <div>
+        <div className="mockup-browser border border-base-300 w-full">
+          <div className="mockup-browser-toolbar">
+            <div className="input">https://daisyui.com</div>
+          </div>
+          <div className="grid place-content-center border-t border-base-300 h-80">
+            Hello!
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default page;
